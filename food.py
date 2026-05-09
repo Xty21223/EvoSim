2f1f90417f1e140ebfad6ba362cefc83b3d5c774
xty21223 <114356098+Xty21223@users.noreply.github.com>
2026-05-09 13:59:25 -0700

    Refactor Cell and Food classes to use Pygame for rendering; update main loop for Pygame integration

--- food.py
+++ food.py
@@ -1,26 +1,16 @@
-from turtle import *
+import pygame
+import config
+
 
 class Food:
-
-		def __init__(self,position):
+		def __init__(self,position,screen):
 				self.x = position[0]
 				self.y = position[1]
 				self.size = 5 #the size also affects the enough of energy gained 
+				self.renderer = screen
 
 		def __repr__(self):
 			return(f"Food (object) at pos: \033[96mX:{self.x}\033[0m , \x1b[0mY:{self.y}\x1b[0m | Size:{self.size}")
 			
 		def render(self, debug = False):
-			pencolor("orange")
-
-			penup()
-			goto(self.x, self.y) 
-			pendown()
-			dot(self.size)
-			penup()
-
-			if debug:
-				pencolor("red")
-				goto(self.x, self.y+5) 
-				pendown()
-				write(f"({self.x},{self.y})")
+			pygame.draw.circle(self.renderer, "red",(self.x, self.y), self.size)
