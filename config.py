b0e4e9976be28cf67c1f472e9373ee11c250abde
xty21223 <114356098+Xty21223@users.noreply.github.com>
2026-05-15 20:40:36 -0700

    Update cell behavior and configuration; adjust health threshold and food amount, enhance UI indicators, and add total cell cap.

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 
 MUTATION_RATE = 10
 DEBUG_ENABLED = False
+if DEBUG_ENABLED: DARKLET = "  |  DEBUG ENABLED"
+else: DARKLET = ""
 WIDTH, HEIGHT = 1280*1.25, 720*1.25
-FOOD_AMOUNT = 40
+FOOD_AMOUNT = 60
 FONT_PREFERENCES = ["consolas", "menlo", "monaco", "couriernew"]
 VERSION_NAME = "alpha"
 VERSION = "1.0"
