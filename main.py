1710bafde937ced93bb77f5e14cfeb0177d1e776
xty21223 <114356098+Xty21223@users.noreply.github.com>
2026-05-16 17:11:04 -0700

    Update cell spawning logic and health management. Fix freeze-clump bug, issue #3.

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ while running:
 	for i in range(len(world.cells)):
 		try:
 			world.cells[i].health -= 0.25
-			if world.cells[i].health <= 0 or len(world.cells)-len(deletions) > 200:
+			if world.cells[i].health <= 0:
 				deletions.append(world.cells[i])
 		except IndexError:
 			print("Surpassed index!")
@@ -73,6 +73,12 @@ while running:
 	for object in deletions:
 		deaths += 1
 		world.cells.remove(object)
+
+	if len(world.cells) > 200:
+		excess = len(world.cells) - 200
+		for _ in range(excess):
+			deaths += 1
+			world.cells.pop()
 
 
 	#checkf for reset	
